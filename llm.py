86eb8db7d1a8a9116723c935a488d548a0a27fd4
kayan9896 <a4ssgit@hotmail.com>
2023-11-01 23:39:31 -0700

    llm

---
+++ llm.py
@@ -0,0 +1,26 @@
+import gradio as gr
+from langchain.prompts import PromptTemplate
+import os
+from langchain.llms import OpenAI
+
+load_dotenv('.env')
+k= os.getenv('key')
+os.environ["OPENAI_API_KEY"] = k
+
+# initialize the models
+llm = OpenAI(
+    model_name="text-davinci-003",
+    openai_api_key= k
+)
+
+
+def chatbot(user_input):
+    # defining a template
+    template = """Question: {question}
+    please provide step by step Answer:
+    """
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    formated_prompt =prompt.format(question=str(user_input))
+    return llm(formated_prompt)
+demo = gr.Interface(fn=chatbot, inputs="text", outputs="text")
+demo.launch()  
