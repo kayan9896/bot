559860c9e8621d1bbd055ffbf94ac1571b363009
kayan9896 <a4ssgit@hotmail.com>
2023-11-14 14:30:52 -0800

    memo

--- llm.py
+++ llm.py
@@ -4,6 +4,9 @@ from langchain.chains import LLMChain
 import os
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory, CombinedMemory, ConversationSummaryMemory
+import time
 
 load_dotenv('.env')
 k= os.getenv('key')
@@ -40,4 +43,49 @@ def coverletter(position, company, skills):
     response = llm(formatted_prompt)
     return response
 
-langchain.debug=True
+
+
+
+# Set up conversation memory
+# This memory will store the last k turns of conversation
+conv_memory = ConversationBufferWindowMemory(
+    memory_key="chat_history_lines",
+    input_key="input",
+    k=1
+)
+
+# This memory will store a summary of the conversation
+summary_memory = ConversationSummaryMemory(llm=OpenAI(), input_key="input")
+
+# Combine the two memories
+memory = CombinedMemory(memories=[conv_memory, summary_memory])
+
+# Define the template for the conversation prompt
+_DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Summary of conversation:
+{history}
+Current conversation:
+{chat_history_lines}
+Human: {input}
+AI:"""
+
+# Create the prompt from the template
+PROMPT = PromptTemplate(
+    input_variables=["history", "input", "chat_history_lines"], template=_DEFAULT_TEMPLATE
+)
+
+# Set up the conversation chain
+# This object will handle the conversation flow
+conversation = ConversationChain(
+    llm=llm,
+    verbose=True,
+    memory=memory,
+    prompt=PROMPT
+)
+chat_history=[]
+def respond(message):
+    bot_message = conversation.run(message)  # Run the user's message through the conversation chain
+    chat_history.append((message, bot_message))  # Append the user's message and the bot's response to the chat history
+    time.sleep(1)  # Pause for a moment
+    return bot_message, chat_history  # Return the updated chat history
