30af1bf827a01b2612b5e64e0eafb76dbb71a1e1
kayan9896 <90473306+kayan9896@users.noreply.github.com>
2024-09-07 04:55:31 +0000

    comment out

--- react/src/App.js
+++ react/src/App.js
@@ -12,7 +12,8 @@ import PaymentModal from './PaymentModal';
 
 function App() {
 
-  let uri='https://legendary-fishstick-67w6q66jwxgh4q49-8000.app.github.dev/'
+  let uri='https://bot-8ejc.onrender.com/'
+  //'https://legendary-fishstick-67w6q66jwxgh4q49-8000.app.github.dev/'
   
   const { 
     isAuthenticated, 
